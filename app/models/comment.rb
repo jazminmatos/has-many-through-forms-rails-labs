0fc8117e921cc3e79012d920ce48aa1ebf8b047e
jazminmatos <jmatos1993@gmail.com>
2021-04-16 10:19:17 -0400

    commit tree be1cc070f21f55768eecfa83ea8e383571db4061

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-
+  accepts_nested_attributes_for :user, reject_if: :all_blank
+  #only creates new records if ALL fields are NOT blank
 end
